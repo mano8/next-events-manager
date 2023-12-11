00f1dfe062ebe2bb02d8b8028ef3524d139174b5
Eli <eli.serra173@gmail.com>
2023-12-11 01:43:44 +0100

    Add user not found page

---
+++ app/users/[userId]/not-found.tsx
@@ -0,0 +1,28 @@
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
+import { buttonVariants } from '@/components/ui/button';
+import PageContainer from '@/src/features/layout/PageContainer';
+import { AlertTriangle } from 'lucide-react';
+import Link from 'next/link';
+
+export default function notFound() {
+  return (
+    <PageContainer>
+      <Alert>
+        <AlertTriangle className="h-4 w-4" />
+        <AlertTitle>404 - User not found</AlertTitle>
+        <AlertDescription>
+          The user you are looking for does not exist. Please check the URL and try
+          again.
+        </AlertDescription>
+        <Link
+          href="/"
+          className={buttonVariants({
+            variant: 'link',
+          })}
+        >
+          Back to home page
+        </Link>
+      </Alert>
+    </PageContainer>
+  );
+}
